1a5e8c86340d3bc6eb280d69d6b77c7a95403a30
Lakshya Pal <pallakshya077@gmail.com>
2026-07-05 21:53:20 +0530

    Add solution: 2965 Find Missing and Repeated Values (Easy)

---
+++ 2965-Find-Missing-and-Repeated-Values/README.md
@@ -0,0 +1,7 @@
+# 2965. Find Missing and Repeated Values
+
+**Difficulty:** Easy
+
+**Tags:** Array, Hash Table, Math, Matrix
+
+**Problem:** https://leetcode.com/problems/find-missing-and-repeated-values/
---
+++ 2965-Find-Missing-and-Repeated-Values/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int> ans;
+        unordered_set<int>s;
+        int n = grid.size();
+        int a,b;
+        int gridSum = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                gridSum+= grid[i][j];
+                if(s.find(grid[i][j]) != s.end()){
+                    a = grid[i][j];
+                    ans.push_back(a);
+                }
+            s.insert(grid[i][j]);
+            }
+        }
+
+        int originalSum = (n*n) * (n*n +1) / 2;
+
+        b = originalSum - gridSum + a;
+        ans.push_back(b);
+
+        return ans;
+    }
+};
