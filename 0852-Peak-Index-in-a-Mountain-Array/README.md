3c70098f0dff12041b14f62a0e34b045fe61744f
Lakshya Pal <pallakshya077@gmail.com>
2026-07-05 21:53:34 +0530

    Add solution: 0852 Peak Index in a Mountain Array (Medium)

---
+++ 0852-Peak-Index-in-a-Mountain-Array/README.md
@@ -0,0 +1,7 @@
+# 0852. Peak Index in a Mountain Array
+
+**Difficulty:** Medium
+
+**Tags:** Array, Binary Search
+
+**Problem:** https://leetcode.com/problems/peak-index-in-a-mountain-array/
---
+++ 0852-Peak-Index-in-a-Mountain-Array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int n = arr.size();
+        int st = 1, end = n - 2;
+        while (st <= end)
+        {
+            int mid = st + (end - st)/2;
+            if (arr[mid - 1] < arr[mid] && arr[mid + 1] < arr[mid]){
+                return mid;
+            }
+            if(arr[mid - 1] < arr[mid] ){
+                st = mid + 1;
+            }
+            if(arr[mid + 1] < arr[mid]){
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+};
