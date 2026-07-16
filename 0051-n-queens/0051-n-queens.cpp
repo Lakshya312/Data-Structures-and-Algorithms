8f45abd4f354a565041e52392a1be53c6cfa5e0b
Lakshya_Pal <pallakshya077@gmail.com>
2026-07-16 17:09:02 +0530

    Time: 1 ms (78.73%), Space: 10.3 MB (59.58%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool isSafe(vector<string> board, int row, int col, int n) {
+    bool isSafe(vector<string> &board, int row, int col, int n) {
         // vertical
         for (int i = 0; i < row; i++) {
             if (board[i][col] == 'Q')
